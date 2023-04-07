240424dac34fb55ec8cd4c30def4a4b5f5f362b0
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-04-07 23:22:40 +0530

    push

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4298311150771-5069975416550-JB52bGPa7fDVjFZxJK7wb0ex")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A0528FVCPRQ-5062015764695-7b8831c5517a8fe146d8e05b07136b63ed29a776f737fc834674735ee105cf1c")
+	os.Setenv("SLACK_BOT_TOKEN", "")
+	os.Setenv("SLACK_APP_TOKEN", "")
 
 	// Create Bot
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
